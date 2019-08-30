fd7e0bf81c0a1b663fbb59f05d41aa9084bc1916
githubcyj <2503975458@qq.com>
2019-08-30 17:51:15 +0800

    20190830

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 /**
  * 应用根目录
  */
+
+ import React from 'react'
+ import { Button } from 'antd';
+
+class App extends React.Component{
+    render(){
+        return ( 
+            <Button type="primary">Primary</Button>
+        )
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 /**
  * 入口
  */
+
+ import React from 'react'
+ import ReactDOM from 'react-dom'
+
+ import APP from'./App'
+
+ //将APP组件标签渲染到index页面的div上
+ ReactDOM.render(<APP />, document.getElementById('root'))
